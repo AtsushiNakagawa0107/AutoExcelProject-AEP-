5af74c0847b152909d907a09b3b640d925059e81
佐藤大己 <matikado3594@gmail.com>
2024-01-27 23:32:40 +0900

    新しいファイルの作成

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const port = 3000;
+const opn = require('opn'); // ここを修正
+
+app.use(express.static(path.join(__dirname, 'public'))); // 'public' ディレクトリに静的ファイルを配置
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html')); // 'index.html' は開きたいHTMLファイル
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+  opn(`http://localhost:${port}`);
+});
