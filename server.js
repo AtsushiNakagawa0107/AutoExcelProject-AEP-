a5148a63a6efb088664e49ef2e6e801908269935
佐藤大己 <matikado3594@gmail.com>
2024-01-28 10:13:25 +0900

    ファイルの作成

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 const app = express();
 const port = 3000;
-const opn = require('opn'); // ここを修正
+const opn = require('opn'); 
 
 app.use(express.static(path.join(__dirname, 'public'))); // 'public' ディレクトリに静的ファイルを配置
 
